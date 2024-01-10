829fd0a95a0bc7d400ede2f36de89099cdf03429
criissdm126 <128620048+criissdm126@users.noreply.github.com>
2024-01-10 11:52:02 +0100

    10/01/2024

--- Fragmentdinamico/app/src/main/java/com/example/fragmentdinamico/MainActivity.java
+++ Fragmentdinamico/app/src/main/java/com/example/fragmentdinamico/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.fragmentdinamico;
 
-
 import android.os.Bundle;
 import android.view.View;
-
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +14,7 @@ import androidx.fragment.app.FragmentTransaction;
 public class MainActivity extends AppCompatActivity {
     FragmentTransaction transaction;
     Fragment fragmentInicio, fragmentRojo, fragmentVerde;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,17 +23,33 @@ public class MainActivity extends AppCompatActivity {
         fragmentRojo = new FragmentRojo();
         fragmentVerde = new FragmentVerde();
 
-        getSupportFragmentManager().beginTransaction().add(R.id.contenedorFragmentos,fragmentInicio).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.contenedorFragmentos, fragmentInicio).commit();
     }
-    public void onClick(View view){
+
+    public void onClick(View view) {
         transaction = getSupportFragmentManager().beginTransaction();
         if (view.getId() == R.id.btnRojo) {
-            transaction.replace(R.id.contenedorFragmentos,fragmentRojo).commit();
-        }
-        else if (view.getId() == R.id.btnVerde) {
-            transaction.replace(R.id.contenedorFragmentos,fragmentVerde).commit();
+            transaction.replace(R.id.contenedorFragmentos, fragmentRojo).commit();
+        } else if (view.getId() == R.id.btnVerde) {
+            transaction.replace(R.id.contenedorFragmentos, fragmentVerde).commit();
         }
     }
 
+    @Override
+    public void onBackPressed() {
+        // Obtener el fragmento actual
+        super.onBackPressed();
+        Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.contenedorFragmentos);
 
+        // Verificar si estamos en el fragmento de inicio
+        if (currentFragment instanceof FragmentInicio) {
+            // Si estamos en el fragmento de inicio, realizar las acciones deseadas
+            // Por ejemplo, puedes mostrar un mensaje o realizar alguna acción específica.
+            // En este caso, simplemente finalizamos la actividad.
+            finish();
+        } else {
+            // Si no estamos en el fragmento de inicio, volver al fragmento de inicio.
+            getSupportFragmentManager().beginTransaction().replace(R.id.contenedorFragmentos, fragmentInicio).commit();
+        }
+    }
 }
